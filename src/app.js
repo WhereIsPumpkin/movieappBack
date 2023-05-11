56e8427d6c671809c5dfcf1c8bb0bd22ee6afdd9
WhereIsPumpkin <sgogr2021@agruni.edu.ge>
2023-05-11 08:28:12 +0400

    feat: add cors

--- src/app.js
+++ src/app.js
@@ -1,15 +1,16 @@
 import express from "express";
 import connect from "./database/mongo.js";
+import cors from "cors";
 import { createUser } from "./controllers/userController.js";
 import dotenv from "dotenv";
 
 dotenv.config();
-
 connect();
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.post("/register", createUser);
 
