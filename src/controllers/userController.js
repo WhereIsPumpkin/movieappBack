7da7f4e1327023d724b3461eae635dffb1c76855
WhereIsPumpkin <sgogr2021@agruni.edu.ge>
2023-05-13 12:30:09 +0400

    feat: add user login functionality

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -80,3 +80,20 @@ export const confirmEmail = async (req, res) => {
     res.status(500).json({ message: "Error confirming email" });
   }
 };
+
+export const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const existingUser = await User.findOne({ email });
+
+    if (!existingUser || existingUser.password !== password) {
+      return res.status(400).json({ message: "Invalid email or password" });
+    }
+
+    res.status(200).json({ message: "Login successful" });
+  } catch (error) {
+    console.error("Error logging in user:", error);
+    res.status(500).json({ message: "Error logging in user" });
+  }
+};
